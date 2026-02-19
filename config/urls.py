2c0f730a593dc65927079c81620f2bd352868c0e
THEC <thecjoborder@gmail.com>
2026-02-19 19:03:55 +0100

    Added root redirect to login page

--- config/urls.py
+++ config/urls.py
@@ -1,11 +1,20 @@
 from django.contrib import admin
-from django.urls import path, include  # include is required to link app URLs
+from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.shortcuts import redirect
+
+
+# 🔁 Redirect root domain to login page
+def root_redirect(request):
+    return redirect('/accounts/login/')
+
 
 urlpatterns = [
+    path('', root_redirect),  # 👈 THIS handles www.thecworkflow.com
+
     path('admin/', admin.site.urls),
-    path('accounts/', include('django.contrib.auth.urls')),  # 👈 ADD THIS
+    path('accounts/', include('django.contrib.auth.urls')),
     path('dashboard/', include('dashboards.urls')),
 ]
 
